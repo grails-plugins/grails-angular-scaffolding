658de26fbd8c09788deb208683a5548304317890
jameskleeh <james.kleeh@gmail.com>
2017-03-16 09:41:50 -0400

    Progress on the docs. Added ngInstallTemplates command

---
+++ grails-app/commands/angular2/scaffolding/NgInstallTemplates.groovy
@@ -0,0 +1,27 @@
+package angular2.scaffolding
+
+import grails.dev.commands.GrailsApplicationCommand
+import org.grails.io.support.Resource
+import org.grails.io.support.SpringIOUtils
+
+class NgInstallTemplates implements GrailsApplicationCommand {
+
+    @Override
+    boolean handle() {
+        try {
+            templates("angular2/**/*").each { Resource r ->
+                String path = r.URL.toString().replaceAll(/^.*?META-INF/, "src/main")
+                if (path.endsWith('/')) {
+                    mkdir(path)
+                } else {
+                    File to = new File(path)
+                    SpringIOUtils.copy(r, to)
+                    println("Copied ${r.filename} to location ${to.canonicalPath}")
+                }
+            }
+        } catch (e) {
+            println e
+        }
+    }
+
+}
